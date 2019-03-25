69ca374809de6d91d587d5df667766785395edad
Bruce Gordon <bruce@focusls.com>
2019-03-25 16:43:06 -0400

    1

---
+++ CommerceTraining/Models/Catalog/FashionNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace CommerceTraining.Models.Catalog
+{
+    [ContentType(DisplayName = "FashionNode", GUID = "15f08ae6-2056-4f94-9080-b87f39e92a90", Description = "")]
+    public class FashionNode : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
